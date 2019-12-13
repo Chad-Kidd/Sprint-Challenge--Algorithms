935b16f86adb8d94bef12a3b5fe99725236db152
chad kidd <ckiddwebdev@gmail.com>
2019-12-13 13:49:02 -0800

    th completed

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -8,3 +8,18 @@ def count_th(word):
     # TBC
     
     pass
+if len(word) == 2:	        
+    if word[:2] == 'th':
+      return 1
+    else:
+      return 0
+	    # empty string etc
+ elif len(word) < 2:
+	return 0
+	else:
+	if word[0:2] == 'th':
+	return 1 + count_th(word[1:])
+	else:
+	return 0 + count_th(word[1:])
+	
+		print(count_th('mikeththyson'))
